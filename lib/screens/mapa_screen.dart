980e528ccb92bfba442fd7cbeaca697aff64bee6
Marek Daněk <MarekDanek05@seznam.cz>
2026-02-23 11:05:39 +0100

    Přidání mapy na hlavní obrazovku a rozdělení kódu do podsložek

---
+++ lib/screens/mapa_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+class MapaScreen extends StatelessWidget {
+  const MapaScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BOD. UJEP - Mapa'),
+        backgroundColor: Colors.amber,
+      ),
+      body: FlutterMap(
+        options: MapOptions(
+          initialCenter: const LatLng(50.6607, 14.0322),
+          initialZoom: 13.0,
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+            userAgentPackageName: 'cz.ujep.bod',
+          ),
+          MarkerLayer(
+            markers: [
+              Marker(
+                point: const LatLng(50.6650, 14.0322),
+                width: 50,
+                height: 50,
+                child: GestureDetector(
+                  onTap: () {
+                    print("Klik na bod!");
+                  },
+                  child: const Icon(Icons.location_on, color: Colors.red, size: 40),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
